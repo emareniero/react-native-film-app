6fd7ae22adc9bf75f91fe3f7c49c13ae23937f55
Emanuel Reniero <emareniero@gmail.com>
2023-05-02 21:23:23 +0100

    Primer commit Peliculas App

---
+++ src/api/movieDB.tsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+
+const movieDB = axios.create({
+    baseURL: "https://api.themoviedb.org/3/movie",
+    params: {
+        api_key: "3f6dde85954e77a6bc373f1c151e1c95",
+        language: "es-ES"
+    }
+});
+
+export default movieDB;
+
+
+
+
+
