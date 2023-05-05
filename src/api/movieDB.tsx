90d30198a4cd77c4a046c0ab2da610625cf87587
Emanuel Reniero <emareniero@gmail.com>
2023-05-05 18:37:20 +0100

    Carrucel principal de películas creado

--- src/api/movieDB.tsx
+++ src/api/movieDB.tsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-
 const movieDB = axios.create({
     baseURL: "https://api.themoviedb.org/3/movie",
     params: {
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,21 +1,44 @@
 import React from 'react';
-import {ActivityIndicator, Text, View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+
+import Carousel from 'react-native-snap-carousel';
+
+import {ActivityIndicator, Dimensions, View} from 'react-native';
 import {useMovies} from '../hooks/useMovies';
+import {MoviePoster} from '../components/MoviePoster';
+
+const {width: windowWidth} = Dimensions.get('window');
 
 export const HomeScreen = () => {
   const {peliculasEnCine, isLoading} = useMovies();
+  const {top} = useSafeAreaInsets();
 
   if (isLoading) {
     return (
-      <View style={{ flex: 1, justifyContent:"center", alignItems:"center"}}>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <ActivityIndicator color="red" size={100} />
       </View>
     );
   }
 
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={{marginTop: top + 20}}>
+      {/* <MoviePoster
+        movie={peliculasEnCine[0]}
+      /> */}
+
+      <View
+        style={{
+          height: 440,
+          // backgroundColor: "red"
+        }}>
+        <Carousel
+          data={peliculasEnCine}
+          renderItem={({item}: any) => <MoviePoster movie={item} />}
+          sliderWidth={windowWidth}
+          itemWidth={300}
+        />
+      </View>
     </View>
   );
 };
