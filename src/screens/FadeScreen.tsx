c6bf1858d0d40bfd988693659f946b1b9e30cd15
Emanuel Reniero <emareniero@gmail.com>
2023-05-06 15:15:13 +0100

    Correción pequeños detalles

--- src/screens/FadeScreen.tsx
+++ src/screens/FadeScreen.tsx
@@ -27,11 +27,11 @@ export const FadeScreen = () => {
 
         <Button
             title="FadeIn"
-            onPress={fadeIn}
+            onPress={() => fadeIn()}
         />
         <Button
             title="FadeOut"
-            onPress={fadeOut}
+            onPress={() => fadeOut()}
         />
 
     </View>
