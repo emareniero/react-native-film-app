37d3cb5d64fe7fc04a3f42be185b339196e338d1
Emanuel Reniero <emareniero@gmail.com>
2023-05-06 14:49:37 +0100

    Fondo cambia segùn portada

--- App.tsx
+++ App.tsx
@@ -2,15 +2,22 @@ import 'react-native-gesture-handler';
 
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import { Navigation } from './src/navigation/Navigation';
-import { Screen } from 'react-native-screens';
-import { FadeScreen } from './src/screens/FadeScreen';
+import {Navigation} from './src/navigation/Navigation';
+import {Screen} from 'react-native-screens';
+import {FadeScreen} from './src/screens/FadeScreen';
+import {GradientProvider} from './src/context/GradientContext';
+
+const AppState = ({children}: any) => {
+  return <GradientProvider>{children}</GradientProvider>;
+};
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Navigation/>
-      {/* <FadeScreen/> */}
+      <AppState>
+        <Navigation />
+        {/* <FadeScreen/> */}
+      </AppState>
     </NavigationContainer>
   );
 };
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 import Carousel from 'react-native-snap-carousel';
@@ -10,12 +10,14 @@ import {MoviePoster} from '../components/MoviePoster';
 import {HorizontalSlider} from '../components/HorizontalSlider';
 import {GradientBackground} from '../components/GradientBackground';
 import { getImageColors } from '../helpers/getColores';
+import { GradientContext } from '../context/GradientContext';
 
 const {width: windowWidth} = Dimensions.get('window');
 
 export const HomeScreen = () => {
   const {nowPlaying, popular, upcoming, topRated, isLoading} = useMovies();
   const {top} = useSafeAreaInsets();
+  const {setMainColors} = useContext(GradientContext)
 
   if (isLoading) {
     return (
@@ -29,8 +31,8 @@ export const HomeScreen = () => {
     const movie = nowPlaying[index];
     const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
-    const [primary, secondary]  = await getImageColors(uri)
-    console.log({primary, secondary})
+    const [primary = "green", secondary = "orange"]  = await getImageColors(uri)
+    setMainColors({ primary, secondary})
   };
 
   return (
